afb286d0e6fbcc46ab7e361deb5c2675af5c4afb
JD <katariaronak95@gmail.com>
2020-04-17 16:55:49 +0530

    Version:1.1.0

--- DVRscripts/script3.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-while true
-do
-    sudo ffmpeg -i "rtsp://python:India123@192.168.75.18/Streaming/Channels/1401" -c copy -map 0 -f segment -segment_time 600 -strftime 1 "/home/ubuntu/Desktop/PIKIT-5_WORKING/DVRJetson/DVR/Parking/%s.mp4"
-    sudo chmod -R 777 *
-done
